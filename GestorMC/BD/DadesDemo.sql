3c4e61ed7a605e7e4bbf486359c5c788e5bf5478
MandyKo <61383291+MandyKo@users.noreply.github.com>
2026-05-12 13:32:36 +0200

    Afegit mes habilitats i efectes a les dades demo

---
+++ GestorMC/BD/DadesDemo.sql
@@ -0,0 +1,145 @@
+-- =========================================================================
+-- 1. POBLACIÓ DE CATÀLEGS (Els "Enums")
+-- =========================================================================
+
+INSERT INTO Objectiu (id, nom) VALUES 
+(1, 'Tu'), 
+(2, 'Enemic'), 
+(3, 'Aliat'),
+(4, 'Equip aliat'), 
+(5, 'Equip enemic');
+
+INSERT INTO Taula_estadistiques (id, nom, icona) VALUES 
+(1, 'Vida', 'https://api.icon/hp.png'),
+(2, 'Atac', 'https://api.icon/atk.png'),
+(3, 'Defensa', 'https://api.icon/def.png'),
+(4, 'Velocitat', 'https://api.icon/spd.png');
+
+INSERT INTO Taula_estats (id, nom, descripcio, icona) VALUES 
+(1, 'Enverinat', 'Dany per torn acumulat', 'https://api.icon/poison.png'),
+(2, 'Cremat', 'Dany de foc i redueix atac', 'https://api.icon/fire.png'),
+(3, 'Atordit', 'El personatge perd el torn', 'https://api.icon/stun.png');
+
+-- =========================================================================
+-- 2. PERSONATGES (Herois i Enemics)
+-- =========================================================================
+
+-- Herois (Jugable = TRUE)
+INSERT INTO Personatge (id, atac, defensa, experiencia, velocitat, vida, nom, descripcio, imatge, icona, jugable) VALUES 
+(1, 150, 100, 0, 110, 255, 'Jeffrey Epstein', 'Segueix viu en Palm Beach', 'epstein.png', 'epsteinIcon.png', TRUE),
+(2, 200, 75, 0, 90, 240, 'Donald Trump', 'Demostra que tothom pot ser president', 'trump.png', 'trumpIcon.png', TRUE);
+
+-- Enemics (Jugable = FALSE)
+INSERT INTO Personatge (id, atac, defensa, experiencia, velocitat, vida, nom, descripcio, imatge, icona, jugable) VALUES 
+(3, 45, 30, 20, 130, 50, 'Mercader Jueu', 'Extremadament ràpid amb els sheckels caiguts', 'jewBoss.png', 'jewIcon.PNG', FALSE),
+(4, 180, 120, 150, 40, 250, 'Jueu Final Form', 'La victòria se li va prometre fa 6700 anys', 'megajewBoss.png', 'megajewIcon.PNG', FALSE);
+
+-- =========================================================================
+-- 3. HABILITATS (Màxim 4 per personatge)
+-- =========================================================================
+
+-- Accions per als Personatges Jugables (Per provar estats)
+INSERT INTO Accio (id, id_objectiu, nom, descripcio, icona) VALUES 
+(10, 2, 'Còctel a la Illa', 'Un combinat sospitós que enverina l''enemic', 'cocktail.png'),
+(11, 2, 'Xantatge de Menors', 'Deixa l''enemic atordit per la pressió social', 'xantatge.png'),
+(20, 2, 'Twitter Roast', 'Un tuit tan incendiari que crema l''enemic', 'twitter.png'),
+(21, 2, 'Fake News', 'Confon l''enemic fins a atordir-lo', 'fakenews.png');
+
+-- Accions per al Mercader Jueu (ID 3) i Jueu Final Form (ID 4)
+INSERT INTO Accio (id, id_objectiu, nom, descripcio, icona) VALUES 
+(60, 5, 'Plaga de Llagostes', 'Invoca un eixam que danya a tot l''equip enemic', 'llagostes.png'),
+(61, 2, 'Clarí del Xofar', 'Un so sacre que pot atordir l''enemic', 'xofar.png'),
+(62, 1, 'Pacte d''Abraham', 'Augmenta la defensa pròpia dràsticament', 'pacte.png'),
+(63, 2, 'Interès Compost', 'Danya l''enemic i drena la seva velocitat', 'interes.png');
+
+-- Assignació d'habilitats a Jeffrey Epstein (ID 1) -> Prova Enverinat (1) i Atordit (3)
+INSERT INTO Habilitat (id_accio, id_personatge) VALUES 
+(10, 1), 
+(11, 1);
+
+-- Assignació d'habilitats a Donald Trump (ID 2) -> Prova Cremat (2) i Atordit (3)
+INSERT INTO Habilitat (id_accio, id_personatge) VALUES 
+(20, 2), 
+(21, 2);
+
+-- Assignació d'habilitats al Mercader Jueu (ID 3)
+INSERT INTO Habilitat (id_accio, id_personatge) VALUES 
+(61, 3), 
+(63, 3);
+
+-- Assignació d'habilitats al Jueu Final Form (ID 4)
+INSERT INTO Habilitat (id_accio, id_personatge) VALUES 
+(60, 4), 
+(62, 4);
+
+-- =========================================================================
+-- 4. ÍTEMS I INVENTARI
+-- =========================================================================
+
+-- Crear el Vi de Quidush (Cura i bufa l'atac)
+INSERT INTO Accio (id, id_objectiu, nom, descripcio, icona) VALUES 
+(80, 1, 'Vi de Quidush', 'Vi sagrat que cura i dona força', 'vi.png');
+
+INSERT INTO Item (id_accio) VALUES (80);
+
+-- =========================================================================
+-- 5. EFECTES
+-- =========================================================================
+
+-- Efectes Jugables:
+-- 10: Còctel a la Illa (Enverinat ID 1)
+INSERT INTO Efecte (id, id_accio, nom, probabilitat, id_estadistica, id_estat, quantitat, duracio, esAfegir) VALUES 
+(100, 10, 'Venè d''illa', 100, NULL, 1, 1, 3, TRUE);
+
+-- 11: Xantatge de Menors (Atordit ID 3)
+INSERT INTO Efecte (id, id_accio, nom, probabilitat, id_estadistica, id_estat, quantitat, duracio, esAfegir) VALUES 
+(110, 11, 'Paràlisi social', 80, NULL, 3, 1, 1, TRUE);
+
+-- 20: Twitter Roast (Cremat ID 2)
+INSERT INTO Efecte (id, id_accio, nom, probabilitat, id_estadistica, id_estat, quantitat, duracio, esAfegir) VALUES 
+(200, 20, 'Burn verbal', 100, NULL, 2, 1, 2, TRUE);
+
+-- 21: Fake News (Atordit ID 3)
+INSERT INTO Efecte (id, id_accio, nom, probabilitat, id_estadistica, id_estat, quantitat, duracio, esAfegir) VALUES 
+(210, 21, 'Confusió mediàtica', 70, NULL, 3, 1, 1, TRUE);
+
+
+-- Efectes Enemics:
+-- 60: Plaga de Llagostes (Dany de vida 1)
+INSERT INTO Efecte (id, id_accio, nom, probabilitat, id_estadistica, id_estat, quantitat, duracio, esAfegir) VALUES 
+(600, 60, 'Dany d''eixam', 100, 1, NULL, 40, 0, FALSE);
+
+-- 61: Clarí del Xofar (Atordit ID 3)
+INSERT INTO Efecte (id, id_accio, nom, probabilitat, id_estadistica, id_estat, quantitat, duracio, esAfegir) VALUES 
+(610, 61, 'Atordiment sonor', 60, NULL, 3, 1, 1, TRUE);
+
+-- 62: Pacte d'Abraham (Defensa ID 3)
+INSERT INTO Efecte (id, id_accio, nom, probabilitat, id_estadistica, id_estat, quantitat, duracio, esAfegir) VALUES 
+(620, 62, 'Protecció divina', 100, 3, NULL, 50, 3, TRUE);
+
+-- 63: Interès Compost (Vida ID 1 + Velocitat ID 4)
+INSERT INTO Efecte (id, id_accio, nom, probabilitat, id_estadistica, id_estat, quantitat, duracio, esAfegir) VALUES 
+(630, 63, 'Cobrament', 100, 1, NULL, 30, 0, FALSE),
+(631, 63, 'Llast financer', 100, 4, NULL, 20, 2, FALSE);
+
+-- Efectes del Vi: Cura Vida i puja Atac (ID 2)
+INSERT INTO Efecte (id, id_accio, nom, probabilitat, id_estadistica, id_estat, quantitat, duracio, esAfegir) VALUES 
+(700, 80, 'Benedicció de salut', 100, 1, NULL, 50, 0, TRUE),
+(701, 80, 'Eufòria sagrada', 100, 2, NULL, 15, 2, TRUE);
+
+-- =========================================================================
+-- 6. NIVELLS I RECOMPENSES
+-- =========================================================================
+
+-- Nivell 1: Sinagoga (Ordre 1)
+INSERT INTO Nivell (id, ordre, fons, id_enemic_1, id_enemic_2) VALUES 
+(1, 1, 'level1.jpg', 3, 3);
+
+-- Nivell 2: Infern Jueu (Ordre 2)
+INSERT INTO Nivell (id, ordre, fons, id_enemic_1, id_enemic_2, id_enemic_3) VALUES 
+(2, 2, 'level2.jpg', 3, 4, 3);
+
+-- Inventari inicial d'ítems
+INSERT INTO Personatge_Item (id_personatge, id_item, quantitat_stock) VALUES 
+(1, 80, 2),
+(2, 80, 2);
